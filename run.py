5e28aa04e6032c5fad8dfbc70882bab104581ed2
Rupert <rupertjhewson@gmail.com>
2019-11-21 16:25:19 +0000

    update from error

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "render_template("index.html)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP',"0.0.0.0"),
