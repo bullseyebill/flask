00efcc377b7bce3c464bfeaa884ed31cf940175c
Rupert <rupertjhewson@gmail.com>
2019-11-21 17:37:33 +0000

    updates template literals!

--- run.py
+++ run.py
@@ -15,7 +15,11 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")     
+    return render_template("contact.html")   
+
+@app.route("/Careers")
+def careers():
+    return render_template("careers.html")      
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP',"0.0.0.0"),
