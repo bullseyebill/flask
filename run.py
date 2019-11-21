65154b68f877a2d6e911e61edcffec46f3405d05
Rupert <rupertjhewson@gmail.com>
2019-11-21 16:04:27 +0000

    py file

--- run.py
+++ run.py
@@ -9,6 +9,6 @@ def hello():
     return "Hello World"
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP',"0.0.0.0"),
+            port=int(os.environ.get('PORT',8000)),
             debug=True)
